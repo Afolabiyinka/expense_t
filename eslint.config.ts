4c2e5d73981a1391167b52de021e7e86654279a2
Afolabi Olayinka <Afolabiyinka738@gmail.com>
2025-11-22 16:42:51 +0100

    Removing redundant js code

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,38 +1,57 @@
 import js from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
+// @ts-ignore
 import reactHooks from "eslint-plugin-react-hooks";
+
 import reactRefresh from "eslint-plugin-react-refresh";
 
-export default [
-  { ignores: ["dist"] },
-  {
-    files: ["**/*.{js,jsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
+export default {
+  ignores: ["dist"],
+
+  overrides: [
+    {
+      files: ["**/*.{js,jsx}"],
+
+      languageOptions: {
         ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
         sourceType: "module",
+        globals: globals.browser,
+        parserOptions: {
+          ecmaFeatures: { jsx: true },
+        },
+      },
+
+      settings: {
+        react: { version: "detect" }, // automatically detect React version
+      },
+
+      plugins: {
+        react,
+        "react-hooks": reactHooks,
+        "react-refresh": reactRefresh,
+      },
+
+      rules: {
+        // Base JS rules
+        ...js.configs.recommended.rules,
+
+        // React rules
+        ...react.configs.recommended.rules,
+        ...react.configs["jsx-runtime"].rules,
+
+        // React Hooks rules
+        ...reactHooks.configs.recommended.rules,
+
+        // React Refresh
+        "react-refresh/only-export-components": [
+          "warn",
+          { allowConstantExport: true },
+        ],
+
+        // Custom overrides
+        "react/jsx-no-target-blank": "off",
       },
     },
-    settings: { react: { version: "18.3" } },
-    plugins: {
-      react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-      ...reactHooks.configs.recommended.rules,
-      "react/jsx-no-target-blank": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
-  },
-];
+  ],
+};
